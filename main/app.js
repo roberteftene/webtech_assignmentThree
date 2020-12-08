0caac0c627961b6e7771931a482405250f5734c3
roberteftene <roberteftene25@gmail.com>
2020-12-08 17:37:36 +0200

    implemented post method /food-items

--- main/app.js
+++ main/app.js
@@ -4,87 +4,119 @@ const Sequelize = require('sequelize')
 
 const mysql = require('mysql2/promise')
 
-// TODO: change the credentials to fit your own
-// if user does not have the right to create, run (as root): GRANT ALL PRIVILEGES ON *.* TO 'app'@'localhost';
-const DB_USERNAME = 'app1'
-const DB_PASSWORD = 'welcome123'
+const DB_USERNAME = 'root'
+const DB_PASSWORD = 'pass'
 
 let conn
 
 mysql.createConnection({
-    user : DB_USERNAME,
-    password : DB_PASSWORD
-})
-.then((connection) => {
-    conn = connection
-    return connection.query('CREATE DATABASE IF NOT EXISTS tw_homework')
-})
-.then(() => {
-    return conn.end()
-})
-.catch((err) => {
-    console.warn(err.stack)
-})
+        user: DB_USERNAME,
+        password: DB_PASSWORD
+    })
+    .then((connection) => {
+        conn = connection
+        return connection.query('CREATE DATABASE IF NOT EXISTS tw_homework')
+    })
+    .then(() => {
+        return conn.end()
+    })
+    .catch((err) => {
+        console.warn(err.stack)
+    })
 
-const sequelize = new Sequelize('tw_homework', DB_USERNAME, DB_PASSWORD,{
-    dialect : 'mysql',
+const sequelize = new Sequelize('tw_homework', DB_USERNAME, DB_PASSWORD, {
+    dialect: 'mysql',
     logging: false
 })
 
 let FoodItem = sequelize.define('foodItem', {
-    name : Sequelize.STRING,
-    category : {
+    name: Sequelize.STRING,
+    category: {
         type: Sequelize.STRING,
         validate: {
             len: [3, 10]
         },
         allowNull: false
     },
-    calories : Sequelize.INTEGER
-},{
-    timestamps : false
+    calories: Sequelize.INTEGER
+}, {
+    timestamps: false
 })
 
 
 const app = express()
-// TODO
+app.use(bodyParser.json());
 
 app.get('/create', async (req, res) => {
-    try{
-        await sequelize.sync({force : true})
-        for (let i = 0; i < 10; i++){
+    try {
+        await sequelize.sync({
+            force: true
+        })
+        for (let i = 0; i < 10; i++) {
             let foodItem = new FoodItem({
                 name: 'name ' + i,
                 category: ['MEAT', 'DAIRY', 'VEGETABLE'][Math.floor(Math.random() * 3)],
-                calories : 30 + i
+                calories: 30 + i
             })
             await foodItem.save()
         }
-        res.status(201).json({message : 'created'})
-    }
-    catch(err){
+        res.status(201).json({
+            message: 'created'
+        })
+    } catch (err) {
         console.warn(err.stack)
-        res.status(500).json({message : 'server error'})
+        res.status(500).json({
+            message: 'server error'
+        })
     }
 })
 
 app.get('/food-items', async (req, res) => {
-    try{
+    try {
         let foodItems = await FoodItem.findAll()
         res.status(200).json(foodItems)
-    }
-    catch(err){
+    } catch (err) {
         console.warn(err.stack)
-        res.status(500).json({message : 'server error'})        
+        res.status(500).json({
+            message: 'server error'
+        })
     }
 })
 
 app.post('/food-items', async (req, res) => {
-    try{
-        // TODO
-    }
-    catch(err){
-        // TODO
+    try {
+
+        if (Object.keys(req.body).length === 0) {
+            return res.status(400).send({
+                "message": "body is missing"
+            })
+        }
+
+        if (!(req.body.hasOwnProperty('name') && req.body.hasOwnProperty('category') && req.body.hasOwnProperty('calories'))) {
+            return res.status(400).send({
+                "message": "malformed request"
+            })
+        }
+        if (parseInt(req.body.calories) < 0) {
+            return res.status(400).send({
+                "message": "calories should be a positive number"
+            })
+        }
+
+        if (req.body.category.length < 3 || req.body.category.length > 10) {
+            return res.status(400).send({
+                "message": 'not a valid category'
+            })
+        }
+        await FoodItem.create(req.body);
+        res.status(201).send({
+            "message": "created"
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            message: 'server error'
+        })
     }
 })
 
